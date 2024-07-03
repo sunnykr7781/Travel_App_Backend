d599853eb39e89fa5413b6fa4bd41657d1db3fac
sunny <sunny129271@gmail.com>
2024-07-03 06:01:51 +0530

    added timestamp of user creation

--- model/user.model.js
+++ model/user.model.js
@@ -5,7 +5,12 @@ const userSchema = new mongoose.Schema({
   number: { type: Number, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-})
+  
+},
+{
+  timestamps : true,
+}
+)
 
 const User = mongoose.model("User", userSchema)
 module.exports = User
